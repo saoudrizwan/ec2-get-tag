87c0677e3a6f5a9224a8608006c8a29d0bcc057a
Saoud Rizwan <hello@saoudmr.com>
2019-09-26 16:20:40 -0400

    Replace smart quotes with normal

--- ec2-get-tag.sh
+++ ec2-get-tag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-result=$(aws ec2 describe-tags –region $(curl -s 169.254.169.254/latest/meta-data/placement/availability-zone | sed ‘s/.$//’ ) –filters “Name=key,Values=$1” “Name=resource-id,Values=$(curl -s 169.254.169.254/latest/meta-data/instance-id)” –output text –query Tags[].Value)
+result=$(aws ec2 describe-tags –region $(curl -s 169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//' ) –filters "Name=key,Values=$1" "Name=resource-id,Values=$(curl -s 169.254.169.254/latest/meta-data/instance-id)" –output text –query Tags[].Value)
 echo "$result"
